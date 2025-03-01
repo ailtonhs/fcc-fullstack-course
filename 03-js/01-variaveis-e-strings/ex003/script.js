8f23bb3d66954a702b7c62313dab6ed562c1c5b0
ailtonhs <ailtonhenrick@outlook.com>
2025-03-01 13:23:31 -0300

    feat: adicionar geração de histórias dinâmicas com variáveis

---
+++ 03-js/01-variaveis-e-strings/ex003/script.js
@@ -0,0 +1,24 @@
+let adjective = 'curioso';
+let noun = 'dragão';
+let verb = 'provocado'
+let place = 'castelo';
+let adjective2 = 'enormes'
+let noun2 = 'livros';
+
+const firstStory = 'Era uma vez um(a) ' + adjective + ' ' + noun + ' que amava comer ' + noun2 + '. O ' + noun + ' vivia em um ' + place + ' e tinha ' + adjective2 + ' narinas que sopravam fogo quando era ' + verb + '.';
+
+console.log('Primeira história: ' + firstStory);
+
+
+adjective = 'travesso';
+noun = 'gato';
+verb = 'espirrava';
+place = 'sótão';
+adjective2 = 'minusculas';
+noun2 = 'panquecas';
+
+const secondStory = 'Era uma vez um(a) ' + adjective + ' ' + noun + ' que amava comer ' + noun2 + '. O ' + noun + ' vivia em um ' + place + ' e tinha ' + adjective2 + ' narinas que sopravam fogo quando era ' + verb + '.';
+
+console.log('Segunda história: ' + secondStory);
+
+
