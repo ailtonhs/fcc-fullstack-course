99fd8176a6f1a54aa09ce05e9a9d870826ca3f95
ailtonhs <ailtonhenrick@outlook.com>
2025-03-01 12:32:14 -0300

    feat: adicionar script de fatos divertidos sobre JavaScript

---
+++ 03-js/01-variaveis-e-strings/ex002/script.js
@@ -0,0 +1,25 @@
+console.log("Olá! Sou seu guia de fatos divertidos sobre codificação!");
+
+let botName = 'TeacheBot';
+let botLocation = 'Internet';
+let favoriteLanguage = 'JavaScript';
+
+console.log('Meu nome é ' + botName + ' e eu moro em ' + botLocation + '.');
+
+console.log('Minha linguagem de programação favorita é ' + favoriteLanguage + '.');
+
+let codingFact = favoriteLanguage + ' é uma linguagem dinamicamente tipada!';
+console.log(codingFact);
+
+codingFact = 'Caso queira unir string ou combinar variáveis e string em ' + favoriteLanguage + ', basta você usar o operador de concatenação +.';
+console.log(codingFact);
+
+codingFact = 'Você não pode usar palavras resevada do ' + favoriteLanguage + ' para criar variáveis.';
+console.log(codingFact);
+
+console.log('Foi divertido compartilhar esses fatos com você. Ate logo! - ' + botName + ' para ' + botLocation + '.');
+
+
+
+
+
